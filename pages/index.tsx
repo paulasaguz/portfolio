d2560125df6a4a883f47220d7d4f3be76ea008bb
Paula Johana <37815293+paulasaguz@users.noreply.github.com>
2022-11-18 17:08:35 -0600

    add global styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 
 import Header from '../components/header'
 import Hero from '../components/hero'
@@ -8,6 +9,12 @@ import MyGlobalStyle from '../styles/global-styles'
 const Home: NextPage = () => {
   return (
     <div>
+      <Head>
+        <title>Pau Code</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
+      </Head>
       <MyGlobalStyle />
       <Header />
       <Hero />
--- styles/global-styles.ts
+++ styles/global-styles.ts
@@ -1,10 +1,16 @@
-import { createGlobalStyle} from "styled-components"
+import { createGlobalStyle } from "styled-components"
 
 const MyGlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
   }
+
+  a, h1, h2, h3, h4, h5, h6, p {
+    font-family: Poppins;
+    font-weight: lighter;
+  }
+
   :root {
   --primary: #8F79EB;
   --secondary: #333333;
