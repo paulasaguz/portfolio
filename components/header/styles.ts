1b02cd30bd1c83a2203f800e45a6ecb4013f2e94
Paula Johana <37815293+paulasaguz@users.noreply.github.com>
2023-01-10 15:36:21 -0500

    add dark mode options

--- components/header/styles.ts
+++ components/header/styles.ts
@@ -20,7 +20,7 @@ const StyledLinkedInIcon = styled(LinkedInIcon)`
 const StyledHeader = styled.header`
   box-shadow: 0px 5px 8px rgba(0, 0, 0, 0.25);
   position: fixed;
-  background: white;
+  background: var(--background);
   top: 0;
   left: 0;
   right: 0;
--- styles/global-styles.ts
+++ styles/global-styles.ts
@@ -1,28 +1,42 @@
 import { createGlobalStyle } from "styled-components"
 
 const MyGlobalStyle = createGlobalStyle`
+  :root {
+    --primary: #8F79EB;
+    --secondary: #333333;
+    --tertiary: #F7F7F7;
+    --background: #fcfcfc;
+    --font: black;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    :root {
+      --secondary: white;
+      --tertiary: #202836;
+      --background: #111827;
+      --font: white;
+    }
+  }
+
   body {
     margin: 0;
     padding: 0;
+    background-color: var(--background);
   }
 
   a, h1, h2, h3, h4, h5, h6 {
     font-family: Poppins;
     font-weight: lighter;
     line-height: 1;
+    color: var(--font);
   }
 
   p {
     font-family: Poppins;
     font-weight: lighter;
     line-height: 1.4;
+    color: var(--font);
   }
-
-  :root {
-  --primary: #8F79EB;
-  --secondary: #333333;
-  --tertiary: #F7F7F7;
-}
 `;
 
 export default MyGlobalStyle;
