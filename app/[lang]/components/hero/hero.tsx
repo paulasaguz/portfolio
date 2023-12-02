521b2f7403d467dfb77e97da20dda40fbc6c05b7
paulasaguz <paulajohanasaguz@gmail.com>
2023-12-02 01:08:28 -0600

    updates

--- app/[lang]/components/hero/hero.tsx
+++ app/[lang]/components/hero/hero.tsx
@@ -21,30 +21,30 @@ function Hero({ dictionary, lang }: {dictionary: any; lang: Locale }): JSX.Eleme
         <div className={s.titleContainer}>
           <h1 className={s.h1}>{dictionary.title}</h1>
           <p className={s.subtitle}>{dictionary.subtitle}</p>
-          <div className='flex flex-wrap gap-3'>
+          <div className='flex flex-wrap gap-4'>
             <a className={s.icon} href="https://twitter.com/paulasaguz" target="_blank">
               <Twitter size='32px' />
-              <figcaption>Twitter</figcaption>
+              <figcaption className='mt-2'>Twitter</figcaption>
             </a>
             <a className={s.icon} href="https://github.com/paulasaguz" target="_blank">
               <Github size='32px' />
-              <figcaption>Github</figcaption>
+              <figcaption className='mt-2'>Github</figcaption>
             </a>
             <a className={s.icon} href="mailto:paulajohanasaguz@gmail.com?subject=Hello There!&body=Hi Paula, so I was looking at your blog and..." target="_blank">
               <Email size='33px'/>
-              <figcaption>Email</figcaption>
+              <figcaption className='mt-2'>Email</figcaption>
             </a>
             <a className={s.icon} href="https://www.youtube.com/@PauCode" target="_blank">
               <Youtube size='34px' />
-              <figcaption>Youtube</figcaption>
+              <figcaption className='mt-2'>Youtube</figcaption>
             </a>
             <a className={s.icon} href="https://www.linkedin.com/in/paulasaguz" target="_blank">
               <LinkedIn size='32px' />
-              <figcaption>Linkedin</figcaption>
+              <figcaption className='mt-2'>Linkedin</figcaption>
             </a>
             <a className={s.icon} href="https://medium.com/@paulajohana" target="_blank">
               <Medium size='32px' />
-              <figcaption>Medium</figcaption>
+              <figcaption className='mt-2'>Medium</figcaption>
             </a>
           </div>
           <p className={s.paragraph}>{dictionary.description}{' '}<Link className="text-primary hover:underline" href={`/${lang}/about`}>{dictionary.link}</Link></p>
